d56ef1484bc9beeaa89b89ccb0d9a469d083f507
Michael Alexandrino Freire <alexandrino.michael07@aluno.ifce.edu.br>
2023-07-30 14:34:18 -0300

    usuario add user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,13 @@ import Instructions from '@/components/Instructions';
 import Card from '@/components/Card';
 
 export default function Home() {
-    
-  const [url, setUrl] = useState<string>('');
+  const [username, setUsername] = useState<string>('');
   const [user, setUser] = useState<any>(null);
 
   const fetchProfileData = async (): Promise<void> => {
     try {
-      const response = await fetch(url);
+      const apiUrl = `https://api.github.com/users/${username}`;
+      const response = await fetch(apiUrl);
       const data = await response.json();
       setUser(data);
     } catch (error) {
@@ -19,29 +19,35 @@ export default function Home() {
   };
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    setUrl(e.target.value);
+    setUsername(e.target.value);
   };
 
   const handleButtonClick = (): void => {
     fetchProfileData();
   };
-    return (
-      <main className='searchProfile'>
-        <h1>Create Your ProfileHub</h1>
-  
-        <input 
-        placeholder='Enter your GitHub API URL...'
-        type="text" 
-        value={url} 
-        onChange={handleInputChange} />
-  
-        <button 
+
+  return (
+    <main className='searchProfile'>
+      <h1>Create Your ProfileHub</h1>
+
+      <input
+        placeholder='Enter your GitHub username...'
+        type="text"
+        value={username}
+        onChange={handleInputChange}
+      />
+
+      <button
         type='button'
-        onClick={handleButtonClick}>Generate Card</button>
-        
-        <Instructions/>
-        
-        {user && <Card user={user} />}
-      </main>
-    );
+        onClick={handleButtonClick}
+      >
+        Generate Card
+      </button>
+
+      <Instructions />
+
+      {user && <Card user={user} />}
+    </main>
+  );
 }
+
