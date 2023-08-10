82cef7247b7fc4b4ee9f6954e159c4e0b4dd26b6
Michael Alexandrino Freire <alexandrino.michael07@aluno.ifce.edu.br>
2023-08-10 08:23:00 -0300

    att header and add button download

--- src/app/card/page.tsx
+++ src/app/card/page.tsx
@@ -20,14 +20,20 @@ export default function CardPage() {
   return (
     <div className="flex flex-col h-screen text-black bg-[#484A55]">
       <div className="w-screen flex justify-between px-10 items-center bg-[#484A55] border-b border-gray-500
-      py-1 fixed">
+      py-2 fixed">
         <a
         href="/"
         className="flex justify-center items-center gap-2">
           <img src="/fav.png" alt="go to home" 
-          className="w-10 rounded-xl"/>
-          <span className="text-base text-blue-300 font-bold">Developer Card</span>
+          className="w-8 rounded-xl border border-zinc-400"/>
+          <span className="text-base text-blue-600 font-bold">Developer Card</span>
         </a>
+        <button 
+        onClick={()=> {
+          alert('Essa função será adicionada em breve!')
+        }}
+        className="text-white text-sm font-bold bg-blue-600 py-2 px-2 rounded-xl border border-transparent focus:border focus:border-red-700"
+        >Baixar Card</button>
       </div>
       <div className="flex justify-between items-center">
         <div>
